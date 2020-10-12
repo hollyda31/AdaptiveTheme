203dcbba71829fa46e6a613353f3dd2738e20b5a
Matt Shillitani <matthew_shillitani@bloomfield.edu>
2020-10-12 12:46:27 -0400

    Adding necessary fiels

---
+++ at_core/forms/ext/extension_clearcache.php
@@ -0,0 +1,12 @@
+<?php
+
+use Drupal\Core\Messenger\MessengerInterface;
+
+/**
+ * Form submit handler for the theme settings form.
+ */
+
+function at_core_submit_extension_clearcache() {
+  drupal_flush_all_caches();
+  \Drupal::messenger()->addMessage(t('Cache cleared.'), 'status');
+}
